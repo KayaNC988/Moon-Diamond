4f3a69f1116410465164c4e4eb6007d5c9a7de5d
Wilfried Gamache <gamawil@outlook.fr>
2026-09-18 11:50:54 +0200

    Initialisation du frontend et RACT et ajout du header Moon Diamond

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header className='header'>
+      <div className='header-logo'>
+<img
+      src="/images/logo_Moon-Diamond.png"
+      alt="Logo Moon Diamond"
+      className="brand-logo"
+    />
+      </div>
+
+      <nav className='navbar'>
+  
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Nos toiles</a></li>
+          <li><a href="#">À propos</a></li>
+          <li><a href="#">Contact</a></li>
+       
+      </nav>
+    </header>
+    <main>
+
+    </main>
+    </>
+  )
+}
+
+export default App
